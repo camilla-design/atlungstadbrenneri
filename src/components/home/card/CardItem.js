ed6926e25535d772c0c7ade888042b1efc9e4092
Camilla Reppen <56583550+camilla-design@users.noreply.github.com>
2021-04-21 12:50:09 +0200

    added cards

---
+++ src/components/home/card/CardItem.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { API_URL } from "../../../constants/api/API_URL";
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+function CardItem() {
+    const API = API_URL + "cards";
+    
+    const [cardInfo, setcardInfo] = useState(null)
+    
+    useEffect(() => {
+        axios.get(API)
+        .then(response => {
+            setcardInfo(response.data)
+            console.log(response.data)
+        })
+    }, [API])
+
+
+    if(cardInfo) {
+        return (
+            <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="http://localhost:3000/static/media/DSC09315.6deb4f3f.jpg" />
+                <Card.Body>
+                <Card.Title>{cardInfo[0].title}</Card.Title>
+                <Card.Text>
+                    {cardInfo[0].description}
+                </Card.Text>
+                <Button variant="dark">Spiseriet</Button>
+            </Card.Body>
+        </Card>
+        <Card className="mt-5" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="http://localhost:3000/static/media/DSC09315.6deb4f3f.jpg" />
+                <Card.Body>
+                <Card.Title>{cardInfo[1].title}</Card.Title>
+                <Card.Text>
+                    {cardInfo[1].description}
+                </Card.Text>
+                <Button variant="dark">Take Away</Button>
+            </Card.Body>
+        </Card>
+
+        </>
+
+        )
+    }
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default CardItem;
--- src/components/home/card/MenuCard.js
+++ src/components/home/card/MenuCard.js
@@ -1,9 +1,13 @@
+import CardItem from './CardItem';
+
 function MenuCard() {
     
     return(
         <div className="container mt-2 p-5">
             <h4>Atlungstad Brenneri menyer</h4>
             <p>God mat betyr en god dag. Menyen vår består av et solid utvalg velsmakende og mettende retter laget av råvarer av ypperste kvalitet.</p>
+            <CardItem />
+
         </div>
     )
 }
