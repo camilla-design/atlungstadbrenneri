6c7f443313b42f9733358f699f8a9bc6eb3670f2
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-05-21 10:05:10 +0200

    fixing bugs

--- src/components/spiseriet/Spiseriet.js
+++ src/components/spiseriet/Spiseriet.js
@@ -2,28 +2,32 @@ import { Component } from "react";
 import SpiserietList from "./SpiserietList";
 import Footer from "../footer/Footer";
 
-
 import image2 from "../../images/sliderImages/bigger-carusel-spiseriet.jpg";
 
 class Spiseriet extends Component {
   render() {
     return (
       <>
-          <div class="hero-container">
-            <img className="d-block w-100 h-100" alt="hero" src={image2} title="credit: Atlungstad Brenneri" />
-          </div>
-          <div className="category-container p-3 pt-5">
-            <h1> - Spiseriet -</h1>
-            <h2>Er du sulten?</h2>
-            <p>
-              Her kan du se alle våre menyer! Enten du vil spise hos oss, eller
-              bestille av vår take away meny.
-            </p>
+        <div class="hero-container">
+          <img
+            className="d-block w-100 h-100"
+            alt="hero"
+            src={image2}
+            title="credit: Atlungstad Brenneri"
+          />
+        </div>
+        <div className="category-container p-3 pt-5">
+          <h1> - Spiseriet -</h1>
+          <h2>Er du sulten?</h2>
+          <p>
+            Her kan du se alle våre menyer! Enten du vil spise hos oss, eller
+            bestille av vår take away meny.
+          </p>
 
-            <div className="pt-4 mb-5">
-              <SpiserietList />
-            </div>
+          <div className="pt-4 mb-5">
+            <SpiserietList />
           </div>
+        </div>
 
         <Footer />
       </>
--- src/components/spiseriet/meny/galleri/MenyGalleri.js
+++ src/components/spiseriet/meny/galleri/MenyGalleri.js
@@ -5,41 +5,59 @@ import meny3 from "../../../../images/hero/meny3.jpg";
 import meny4 from "../../../../images/hero/meny4.jpg";
 
 class MenyGalleri extends Component {
-    render() {
-      return (
-          <div className="gallery">
-
-<div className="mb-4 mt-4 image-gallery">
+  render() {
+    return (
+      <div className="gallery">
+        <div className="mb-4 mt-4 image-gallery">
           <div className="responsive-gallery">
             <a target="_blank" rel="noreferrer" href={meny1}>
-              <img src={meny1} width="600" alt="Meny 1" title="credit: Atlungstad Brenneri" />
+              <img
+                src={meny1}
+                width="600"
+                alt="Meny 1"
+                title="credit: Atlungstad Brenneri"
+              />
             </a>
           </div>
         </div>
         <div className="mb-4 mt-4 image-gallery">
           <div className="responsive-gallery">
             <a target="_blank" rel="noreferrer" href={meny2}>
-              <img src={meny2} width="600" alt="Meny 2" title="credit: Atlungstad Brenneri" />
+              <img
+                src={meny2}
+                width="600"
+                alt="Meny 2"
+                title="credit: Atlungstad Brenneri"
+              />
             </a>
           </div>
         </div>
         <div className="mb-4 mt-4 image-gallery">
           <div className="responsive-gallery">
             <a target="_blank" rel="noreferrer" href={meny3}>
-              <img src={meny3} width="600" alt="Meny 3" title="credit: Atlungstad Brenneri" />
+              <img
+                src={meny3}
+                width="600"
+                alt="Meny 3"
+                title="credit: Atlungstad Brenneri"
+              />
             </a>
           </div>
         </div>
         <div className="mb-4 mt-4 image-gallery">
           <div className="responsive-gallery">
             <a target="_blank" rel="noreferrer" href={meny4}>
-              <img src={meny4} width="600" alt="Meny 4" title="credit: Atlungstad Brenneri" />
+              <img
+                src={meny4}
+                width="600"
+                alt="Meny 4"
+                title="credit: Atlungstad Brenneri"
+              />
             </a>
           </div>
         </div>
-
-
-        </div>
-      )
-    }}
+      </div>
+    );
+  }
+}
 export default MenyGalleri;
